e1d01b95671899fa0d0ed92630907630ce5c9d85
Hamza Ilyas <hamza24061@gmail.com>
2022-03-08 18:13:14 +0500

    Updated

--- app.mjs
+++ app.mjs
@@ -63,6 +63,36 @@ app.post("/webhook", (req, res) => {
       const feelsLike = data.main.feels_like;
       const responseText = `The Current Temperature Of ${cityName} is ${temp}°C. Feels Like ${feelsLike}°C. Humidity is ${humidity}% and Wind is ${wind} km/h.`;
       agent.add(responseText);
+
+      // React App Integration
+
+      console.log("User Request ===>", req.body.Body);
+      const projectId = "weather-app-tfos";
+      const sessionId = req.body.sessionId || "session12345";
+      const query = req.body.text;
+      const languageCode = "en-US";
+
+      // The path to identify the agent that owns the created intent.
+      const sessionPath = sessionClient.projectAgentSessionPath(
+        projectId,
+        sessionId
+      );
+
+      const request = {
+        session: sessionPath, // Session ID, should be unique for every conversation
+        queryInput: {
+          text: {
+            text: query,
+            languageCode: languageCode,
+          },
+        },
+      };
+
+      const responses = await sessionClient.detectIntent(request);
+
+      res.send({
+        text: responses[0].queryResult,
+      });
     } catch (error) {
       console.error("ERROR in API Call ===>: ", error);
     }
